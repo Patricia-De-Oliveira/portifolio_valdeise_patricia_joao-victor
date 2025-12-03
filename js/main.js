b346558a6ce36d7fedd4635d5cc108efa25701e1
Seu Nome Completo <victorifpb78@gmail.com>
2025-12-03 16:23:35 -0300

    feat: Links para redes sociais atulizado, carrosel de imagem, centralização de textos e fontes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,52 @@
-const btnTopo = document.getElementById("btn-topo");
+document.addEventListener("DOMContentLoaded", () => {
+    
+    let slideIndex = 1;
 
-if (btnTopo) {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 280) {
-      btnTopo.style.display = "flex";
-    } else {
-      btnTopo.style.display = "none";
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("carrossel-slide");
+      
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+  
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      
+      if (slides[slideIndex - 1]) {
+        slides[slideIndex - 1].style.display = "block"; 
+        slides[slideIndex - 1].classList.add('fade');
+      }
     }
-  });
 
-  btnTopo.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  });
-}
+    window.plusSlides = function(n) {
+      slideIndex += n;
+      showSlides(slideIndex);
+    }
+    
+    showSlides(slideIndex);
+
+    const btnTopo = document.getElementById("btn-topo");
+
+    if (btnTopo) {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 280) { 
+          btnTopo.style.display = "flex";
+        } else {
+          btnTopo.style.display = "none";
+        }
+      });
+
+      btnTopo.addEventListener("click", () => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth", 
+        });
+      });
+    }
+});
